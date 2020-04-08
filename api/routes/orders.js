10b5ffcbfb4c2a13060d98fc3146f4b2953fec95
Ataa0 <ataa.al.ay@gmail.com>
2020-04-08 22:11:03 +0300

    updated the order routes to exchange data with the db, and created a relation between Product and Order

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,35 +1,100 @@
 const express = require('express');
 const router = express.Router();
-
-
+const mongoose = require('mongoose');
+const Order = require('../models/order');
+const Product = require('../models/product');
 router.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message : 'Orders were fetched.'
+    Order.find()
+    .select('product quantity _id')
+    .exec()
+    .then(orders=>{
+        res.status(200).json({
+            count : orders.length,
+            orders : orders.map(order=>{
+                return {
+                    _id : order._id,
+                    product : order.product,
+                    quantity : order.quantity
+                }
+            })
+        })
+    })
+    .catch((err)=>{
+        res.status(500).json({
+            error : err
+        });
     });
 });
 
 router.get('/:orderId',(req,res,next)=>{
-    res.status(200).json({
-        message : 'Order details',
-        orderId : req.params.orderId
+    Order.findById(req.params.orderId)
+    .exec()
+    .then((order)=>{
+        if(!order){
+            res.status(404).json({
+                message : 'order was not found.'
+            });
+        }else{
+            res.status(200).json({
+                order : {
+                    _id : order._id,
+                    quantity : order.quantity,
+                    product : order.product
+                }
+            });
+        }
+    })
+    .catch((err)=>{
+        error : err
     })
 });
 
 router.post('/',(req,res,next)=>{
-    const order = {
-        productId : req.body.productId,
-        quantity : req.body.quantity
+    Product.findById(req.body.productId)
+    .then(product=>{
+        if(!product){
+            res.status(404).json({
+                message : 'product was not found'
+            });
+        }else{
+        const order = new Order({
+            _id : mongoose.Types.ObjectId(),
+            quantity : req.body.quantity,
+            product : req.body.productId
+        });
+        return order.save();
     }
-    res.status(201).json({
-        message : 'Order was created.',
-        order : order
+    })
+    .then((result)=>{
+        console.log(result);
+        res.status(201).json({
+            createdOrder : {
+                _id : result._id,
+                product : result.product,
+                quantity : result.quantity
+            }
+        });
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.status(500).json({
+            error : err
+        });
     });
 });
 
 router.delete('/:orderId',(req,res,next)=>{
-    res.status(200).json({
-        message : 'Order deleted',
+    console.log('Delete')
+    Order.remove({_id : req.params.orderId})
+    .exec()
+    .then((result)=>{
+        res.status(200).json({
+            message : 'order deleted.'
+        });
     })
+    .catch((err)=>{
+        error : err
+    });
 });
 
 
